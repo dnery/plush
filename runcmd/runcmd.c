c96aefd7c31063ca83744a84d88acf8bffbb5f83
Henrique1792 <freitash1792@gmail.com>
2016-05-29 18:00:02 -0300

    Add Author line

--- runcmd/runcmd.c
+++ runcmd/runcmd.c
@@ -16,7 +16,9 @@
    
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
+
+   
+   Code written by Henrique F. M. Freitas (https://github.com/Henrique1792)
 */
 
 #include <unistd.h>
